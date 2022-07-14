d7e3c00d4ef2b47ad64afe3e8eef984adea590b2
Alex <basurillodesgastorito@gmail.com>
2022-07-14 21:43:24 +0200

    First upload

---
+++ index.js
@@ -0,0 +1,70 @@
+window.addEventListener("scroll", function() {
+    var header = document.querySelector(".scroller");
+    header.classList.toggle("sticky", window.scrollY > 0);
+})  
+
+window.addEventListener("scroll", function() {
+    var header = document.querySelector(".textscroll");
+    header.classList.toggle("sticky", window.scrollY > 0);
+})  
+
+window.addEventListener("scroll", function() {
+    var header = document.querySelector(".downloadcv");
+    header.classList.toggle("sticky", window.scrollY > 0);
+})  
+
+window.addEventListener("scroll", function() {
+    var header = document.querySelector(".btn");
+    header.classList.toggle("sticky", window.scrollY > 0);
+})  
+
+window.addEventListener("scroll", function() {
+    var header = document.querySelector(".btn2");
+    header.classList.toggle("sticky", window.scrollY > 0);
+})  
+
+window.addEventListener("scroll", function() {
+    var header = document.querySelector("#aaa");
+    header.classList.toggle("sticky", window.scrollY > 0);
+})  
+
+window.addEventListener("scroll", function() {
+    var header = document.querySelector("#bbb");
+    header.classList.toggle("sticky", window.scrollY > 0);
+})  
+
+window.addEventListener("scroll", function() {
+    var header = document.querySelector("#ccc");
+    header.classList.toggle("sticky", window.scrollY > 0);
+})
+
+window.addEventListener("scroll", function() {
+    var header = document.querySelector("#ddd");
+    header.classList.toggle("sticky", window.scrollY > 0);
+})
+
+window.addEventListener("scroll", function() {
+    var header = document.querySelector("#eee");
+    header.classList.toggle("sticky", window.scrollY > 0);
+})
+
+window.addEventListener("scroll", function() {
+    var header = document.querySelector("#fff");
+    header.classList.toggle("sticky", window.scrollY > 0);
+})
+
+
+var scene = document.getElementById('reddit');
+var parallaxInstance = new Parallax(scene);
+
+var scene = document.getElementById('twitter');
+var parallaxInstance = new Parallax(scene);
+
+var scene = document.getElementById('discord');
+var parallaxInstance = new Parallax(scene);
+
+var scene = document.getElementById('github');
+var parallaxInstance = new Parallax(scene);
+
+var scene = document.getElementById('Facehold');
+var parallaxInstance = new Parallax(scene);
