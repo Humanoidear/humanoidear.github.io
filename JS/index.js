a67e7a4d75906a43d9700a5d0de13482cc065027
Alex <80858832+Humanoidear@users.noreply.github.com>
2022-11-01 00:50:14 +0100

    Add files via upload

---
+++ JS/index.js
@@ -0,0 +1,30 @@
+/* Header blur on hover*/
+window.addEventListener("scroll", function() {
+    var b = document.querySelector("b");
+    b.classList.toggle("sticky", window.scrollY > 800);
+})
+
+window.addEventListener("scroll", function() {
+    var header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 500);
+})
+
+window.addEventListener("scroll", function() {
+    var opaque = document.querySelector("opaque");
+    opaque.classList.toggle("sticky", window.scrollY > 300);
+})
+
+window.addEventListener("scroll", function() {
+    var c = document.querySelector("c");
+    c.classList.toggle("sticky", window.scrollY > 790);
+})
+
+
+
+
+
+/* Launch Particle-JS*/
+
+window.particlesJS.load('particles-js', 'particles.json', function() {
+    console.log('particles.js loaded - callback');
+});
