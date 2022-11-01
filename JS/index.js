f0eb0b922ef90f323406fd0bacaa922f5fa49858
Alex <80858832+Humanoidear@users.noreply.github.com>
2022-11-01 14:43:25 +0000

    Update index.js

--- JS/index.js
+++ JS/index.js
@@ -26,7 +26,7 @@ window.addEventListener("scroll", function() {
 
 window.addEventListener("scroll", function() {
     var e = document.querySelector("e");
-    e.classList.toggle("sticky", window.scrollY > 790);
+    e.classList.toggle("sticky", window.scrollY > 0);
 })
 
 
