ad2955437e8f424980a625f69aa5020d72837d53
Alex <basurillodesgastorito@gmail.com>
2022-11-05 17:40:19 +0100

    a

--- JS/index.js
+++ JS/index.js
@@ -6,7 +6,7 @@ window.addEventListener("scroll", function() {
 
 window.addEventListener("scroll", function() {
     var header = document.querySelector("header");
-    header.classList.toggle("sticky", window.scrollY > 500);
+    header.classList.toggle("sticky", window.scrollY > 790);
 })
 
 window.addEventListener("scroll", function() {
@@ -29,6 +29,48 @@ window.addEventListener("scroll", function() {
     e.classList.toggle("sticky", window.scrollY > 0);
 })
 
+window.addEventListener("scroll", function() {
+    var f = document.querySelector("f");
+    f.classList.toggle("sticky", window.scrollY > 790);
+})
+
+var TRange=null;
+
+function findString (str) {
+ if (parseInt(navigator.appVersion)<4) return;
+ var strFound;
+ if (window.find) {
+
+  // CODE FOR BROWSERS THAT SUPPORT window.find
+
+  strFound=self.find(str);
+  if (!strFound) {
+   strFound=self.find(str,0,1);
+   while (self.find(str,0,1)) continue;
+  }
+ }
+ else if (navigator.appName.indexOf("Microsoft")!=-1) {
+
+  // EXPLORER-SPECIFIC CODE
+
+  if (TRange!=null) {
+   TRange.collapse(false);
+   strFound=TRange.findText(str);
+   if (strFound) TRange.select();
+  }
+  if (TRange==null || strFound==0) {
+   TRange=self.document.body.createTextRange();
+   strFound=TRange.findText(str);
+   if (strFound) TRange.select();
+  }
+ }
+ else if (navigator.appName=="Opera") {
+  alert ("Opera browsers not supported, sorry...")
+  return;
+ }
+ if (!strFound) alert ("String '"+str+"' not found!")
+ return;
+}
 
 
 
