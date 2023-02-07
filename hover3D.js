645e0826a9dcf32bbcc4604488805176b2ca7ff0
Alex <basurillodesgastorito@gmail.com>
2023-02-07 11:26:08 +0100

    a

--- hover3D.js
+++ hover3D.js
@@ -3,8 +3,8 @@ class Hover3D
     constructor(id)
     {
         this.id = id;
-        this.xOffset = 10;
-        this.yOffset = 10;
+        this.xOffset = 2;
+        this.yOffset = 2;
         this.attack = 0.1;
         this.release = 0.5;
         this.perspective = 700;
